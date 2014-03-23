320a16c35d95c2822227689feb66b373e88463fe
Apoorv Birthare <birthare.sgsits@gmail.com>
2014-03-23 18:18:24 +0530

    Added initial setup created using http://ruby.railstutorial.org/ruby-on-rails-tutorial-book#code-gemfile_sqlite_version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '3bcddf2d4ac34c0c58e337c4c270201ac8b5cd2024abbc139a93d3bdc584ac2d90bed38fe9819f7e903f3744b83d81ccb94456a298a68d966d88ceef2dae1a16'
